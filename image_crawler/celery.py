a286a96fb411f4d0b520fbb5e06b05f8901c0c62
alexTse <harryxie123@hotmail.com>
2020-04-15 10:52:21 +0800

    refactor(celery): Add celery log; modify db backup sh script to sudo;

--- image_crawler/celery.py
+++ image_crawler/celery.py
@@ -1,32 +1,43 @@
 import os
 import time
-import pymysql
+import logging
+import configparser
 from django.conf import settings
 from celery import Celery
-
+from celery.signals import after_setup_logger
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_crawler.settings')
+
+config = configparser.ConfigParser()
+config.read(settings.CONFIG_FILE)
+
 app = Celery('image_crawler')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
+@after_setup_logger.connect
+def setup_loggers(logger, *args, **kwargs):
+    formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(module)s]%(message)s')
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler(os.path.join(settings.BASE_DIR, 'log', 'celery.log'))
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
 @app.task
 def db_backup():
     TIMESTAMP = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-    conn = pymysql.connect(
-        settings.DATABASES['default']['HOST'],
-        settings.DATABASES['default']['USER'],
-        settings.DATABASES['default']['PASSWORD'],
-        settings.DATABASES['default']['NAME']
-    )
-    cur = conn.cursor()
-    cmd = 'docker exec -i 0355 mysqldump -u{} -p{} {} > {}'
+    cmd = 'echo {} | sudo -S docker exec -i 0355 mysqldump -u{} -p{} {} > {}'
     os.system(cmd.format(
+        config.get('celery', 'sudo_password'),
         settings.DATABASES['default']['USER'],
         settings.DATABASES['default']['PASSWORD'],
         settings.DATABASES['default']['NAME'],
         os.path.join(settings.DB_BACKUP_DIR, f'bak_{TIMESTAMP}.sql'),
 
     ))
-    conn.close()
--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@ source $SHELL_FOLDER'/venv/bin/activate'
 nohup gunicorn -c gunicorn.conf.py image_crawler.wsgi >/dev/null 2>&1 &
 echo gunicorn initialized...
 
-nohup celery -A image_crawler worker -l info --beat --scheduler django_celery_beat.schedulers:DatabaseScheduler >/dev/null 2>&1 &
+nohup celery -A image_crawler worker -l info --beat --loglevel=INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler >/dev/null 2>&1 &
 echo celery initialized...
